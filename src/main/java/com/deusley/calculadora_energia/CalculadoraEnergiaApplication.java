5514030f56b2b67ecfa8916519641c597ef942a8
Deuslleyw <Deuslleyw@hotmail.com>
2025-05-18 15:52:44 -0300

    FirstCommit

---
+++ src/main/java/com/deusley/calculadora_energia/CalculadoraEnergiaApplication.java
@@ -0,0 +1,16 @@
+package com.deusley.calculadora_energia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraEnergiaApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/test/java/com/deusley/calculadora_energia/CalculadoraEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.calculadora_energia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
