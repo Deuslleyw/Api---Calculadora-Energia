ff03e4cadb0e4c1cf0e417b6f362a88e29026e74
Deuslleyw <Deuslleyw@hotmail.com>
2025-05-21 22:22:47 -0300

    update/ConectFrontEnd

--- src/main/java/com/deusley/calculadora_energia/controller/CalculadoraController.java
+++ src/main/java/com/deusley/calculadora_energia/controller/CalculadoraController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/")
-@CrossOrigin(origins = "http://localhost:4200")
 public class CalculadoraController {
 
     @Autowired
